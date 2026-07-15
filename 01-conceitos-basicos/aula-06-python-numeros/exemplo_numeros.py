7478805a0abff075bc8e46e772f5c84094499572
Felipe <Felpssalvatore42@gmail.com>
2026-07-15 16:15:39 -0300

    Adicionando arquivo de exemplo

---
+++ 01-conceitos-basicos/aula-06-python-numeros/exemplo_numeros.py
@@ -0,0 +1,23 @@
+import math
+
+numero1 = 10
+numero2 = 5.5
+
+print("Número inteiro:", numero1)
+print("Número decimal:", numero2)
+
+print("\nOperações Matemáticas")
+print("-" * 25)
+
+print("Soma:", numero1 + numero2)
+print("Subtração:", numero1 - numero2)
+print("Multiplicação:", numero1 * numero2)
+print("Divisão:", numero1 / numero2)
+print("Potência:", 2 ** 5)
+print("Resto da divisão:", 10 % 3)
+
+print("\nMódulo Math")
+print("-" * 25)
+
+print("Raiz quadrada de 16:", math.sqrt(16))
+print("Fatorial de 5:", math.factorial(5))
